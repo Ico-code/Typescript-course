4f371c048ea51c32edf08f7540f6b8a025359c1b
Nico Viljanen <nico.nikusti@gmail.com>
2021-04-14 00:03:43 +0300

    Section 3 Basic types

--- types.ts
+++ types.ts
@@ -1,1 +1,15 @@
-var n : number = 1;
+var isWinter : boolean = false;
+
+var count : number = 5;
+
+var Name : string = "Bran";
+
+var names : any[] = ["Jon","Rickon","Arya", 5];
+
+enum Starks {Jon,Bran,Eddard,Catlyn};
+
+var cat : Starks = Starks.Catlyn;
+
+function getName() : void{
+    console.log("Winter is coming!");
+}
